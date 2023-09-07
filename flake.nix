a97f17689827af6bfc296cc71d5693d569742b33
Dmytro Kyrychuk <dmytro@kyrych.uk>
2023-09-07 16:56:54 +0000

    Add devshell with poetry

--- flake.nix
+++ flake.nix
@@ -1,11 +1,25 @@
 {
-  description = "A very basic flake";
+  description = "Application packaged using poetry2nix";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [pkgs.poetry];
+      };
+      formatter = pkgs.alejandra;
+    });
 }
